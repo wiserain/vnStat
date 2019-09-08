d698af3b74cf12e38394b977b6e5aa7b14f4e57c
wiserain <mail275@gmail.com>
2019-09-09 02:34:38 +0900

    plugin_info 추가 - 수정

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # 필수 export
-from plugin import blueprint, menu, plugin_load, plugin_unload
-
+from plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info
 # 필요에 따라 외부에서 호출할 클래스 등록
 # Logic이나 Model 들
 #from logic import Logic
